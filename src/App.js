c45e20db2a52f9641e5f527b7bc93999f34ec172
Julius Dunfoy <juliusdunfoy@ucla.edu>
2022-11-28 19:24:02 -0800

    Initial Organization Page commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/styles/Global";
+import OrganizationPage from "./Pages/OrganizationPage/OrganizationPage";
 
+const theme = {
+  colors: {
+    header: '#fff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px',
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme= {theme}>
+      <GlobalStyles />
+      <OrganizationPage />
+    </ThemeProvider>
+    
   );
 }
 
---
+++ src/components/styles/Global.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Permanent+Marker&family=Poppins&display=swap');
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background: ${({ theme }) => theme.colors.body};
+    color: hsl(192, 100%, 9%);
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.15em;
+    margin: 0;
+  }
+  p {
+    opacity: 0.6;
+    line-height: 1.5;
+    
+  }
+  img {
+    max-width: 100%;
+  }
+  ul {
+    list-style-type: none;
+  }
+  a {
+    text-decoration: none;
+    color: hsl(192, 100%, 9%);
+    
+  }
+  a:hover {
+    color: #ffba00
+  }
+
+  h1{
+    font-family: 'Permanent Marker', cursive;
+  }
+`
+
+export default GlobalStyles
+
