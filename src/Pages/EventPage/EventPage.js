63b91e2e9733564142b6bf0f61f2f940b5800e7b
Julius Dunfoy <julius.dunfoy1@gmail.com>
2022-11-28 23:31:13 -0800

    Reorganized files in preparation for merge with other parts of my development and eventually frontend

--- src/Pages/EventPage/EventPage.js
+++ src/Pages/EventPage/EventPage.js
@@ -1,22 +1,28 @@
-import { Container } from "../../components/styles/Container.styled"
-import { StyledEventLeftColumn, StyledEventRightColumn } from "../../components/styles/EventPage.styled"
+import { Container } from "../../components/global/styles/Container.styled"
+import { StyledEventDate, StyledEventLeftColumn, StyledEventRightColumn, StyledSecurityMessage } from "./styles/EventPage.styled"
 import DJClub from "../../assets/images/dj-club.jpg"
-import { StyledEventImage } from "../../components/styles/EventPage.styled"
-import { StyledEventDescription } from "../../components/styles/EventPage.styled"
-import { StyledEventHeader } from "../../components/styles/EventPage.styled"
-import { StyledEventMainInfo } from "../../components/styles/EventPage.styled"
+import { StyledEventImage } from "./styles/EventPage.styled"
+import { StyledEventDescription } from "./styles/EventPage.styled"
+import { StyledEventHeader } from "./styles/EventPage.styled"
+import { StyledEventMainInfo } from "./styles/EventPage.styled"
 const EventPage = () => {
   return (
     <>
       <Container>
         <StyledEventLeftColumn>
           <StyledEventImage src= {DJClub} />
+          <StyledSecurityMessage>
+              Buzz holds your privacy and security dearly. <br/>
+              Your tickets are secured by advanced encryption <br/>
+              and stored in a personal QR code.
+          </StyledSecurityMessage>
         </StyledEventLeftColumn>
         
         <StyledEventRightColumn>
           <StyledEventMainInfo>
             <StyledEventHeader>Russian Techno Night</StyledEventHeader>
-
+            <h3> Royce Hall </h3>
+            <StyledEventDate> Thu, 4 Nov, 9:30pm</StyledEventDate>
           </StyledEventMainInfo>
 
         </StyledEventRightColumn>
