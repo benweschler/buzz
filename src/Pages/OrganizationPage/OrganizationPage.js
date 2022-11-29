b656eca830fded7a095ffbc5dc8d81156ea72595
Julius Dunfoy <julius.dunfoy1@gmail.com>
2022-11-28 19:34:06 -0800

    Checking email change and missed some modifications

--- src/Pages/OrganizationPage/OrganizationPage.js
+++ src/Pages/OrganizationPage/OrganizationPage.js
@@ -1,54 +1,50 @@
-import Navbar from "../../components/global/Navbar"
-import { Container, StyledLeftColumn, StyledRightColumn } from "../../components/styles/Container.styled"
-import { StyledBanner, StyledBannerImage, StyledBannerImageBlurred, StyledBannerText, StyledEventsHeader, StyledOrganizationDescription, StyledOrganizationEvents, StyledOrgContainer } from "../../components/styles/OrganizationPage.styled"
-import RoyceHall from "../../assets/images/Royce-Hall.jpg"
+import Navbar from "../../components/global/Navbar";
+import { Container } from "../../components/styles/Container.styled";
+import {
+  StyledBanner,
+  StyledBannerImage,
+  StyledBannerImageBlurred,
+  StyledBannerText,
+  StyledOrganizationDescription,
+  StyledOrganizationEvents,
+  StyledOrgContainer,
+  StyledOrgLeftColumn,
+  StyledOrgRightColumn,
+} from "../../components/styles/OrganizationPage.styled";
+import RoyceHall from "../../assets/images/Royce-Hall.jpg";
 
 const OrganizationPage = () => {
   return (
     <>
-        <Navbar />
-        <StyledBanner>
-            <StyledBannerImageBlurred src={RoyceHall}/>  
-            <StyledBannerText>
-                Royce Theater Club    
-            </StyledBannerText> 
-            <StyledBannerImage src={RoyceHall} />
-                
-        </StyledBanner> 
+      <Navbar />
+      <StyledBanner>
+        <StyledBannerImageBlurred src={RoyceHall} />
+        <StyledBannerText>Royce Theater Club</StyledBannerText>
+        <StyledBannerImage src={RoyceHall} />
+      </StyledBanner>
 
+      <StyledOrgContainer>
+        <StyledOrgLeftColumn>
+          <Container>
+            <StyledOrganizationDescription>
+              <p>
+                Welcome to Royce Theater Club! <br />
+                We are happy you're here, Please check out our events!!
+              </p>
+            </StyledOrganizationDescription>
+          </Container>
+        </StyledOrgLeftColumn>
 
-        <StyledOrgContainer>
-            <StyledLeftColumn>
-            <Container>
-                
-                <StyledOrganizationDescription>
-                    <p>
-                        Welcome to Royce Theater Club! <br/>
-                        We are happy you're here, Please check out our events!
-                    </p>
-                </StyledOrganizationDescription>   
-                
-            </Container>
-            
-            </StyledLeftColumn>
-            
-            <StyledRightColumn>
-                <Container>
-                    <StyledOrganizationEvents>
-                        <h2> Our Events:</h2>
-                    
-                    </StyledOrganizationEvents>
-
-                </Container>
-                
-            </StyledRightColumn>
-
-        </StyledOrgContainer>
-       
-        
+        <StyledOrgRightColumn>
+          <Container>
+            <StyledOrganizationEvents>
+              <h2> Our Events:</h2>
+            </StyledOrganizationEvents>
+          </Container>
+        </StyledOrgRightColumn>
+      </StyledOrgContainer>
     </>
-    
-  )
-}
+  );
+};
 
-export default OrganizationPage
+export default OrganizationPage;
--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -1,24 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 1200px;
-    max-width: 100%;
-    margin: 0 auto;
-    padding: 0 20px;
-    justify-content: center;
-`
+  width: 1200px;
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+  justify-content: center;
+`;
 
-export const StyledLeftColumn = styled.div`
-   float: left;
-   width: 45%;
-   text-align: start;
-   padding: 10px;
-   margin: auto;
-`
 
-export const StyledRightColumn = styled.div`
-   float: left;
-   width: 55%;
-   text-align: start;
-   padding-left: 20px;
-`
