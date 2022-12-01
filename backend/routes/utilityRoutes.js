f3dece85e478eaabd977b0bdcd5899a04e8ac5c4
WalterChen352 <111105449+WalterChen352@users.noreply.github.com>
2022-11-30 16:14:58 -0800

    added userRegistered function which should be called whenever an event page is loaded and return whether the user is registered for that page

--- backend/routes/utilityRoutes.js
+++ backend/routes/utilityRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const {
-  filter
+  filter,
+  userRegistered
 } = require('../controllers/utilityController');
 
 const router = express.Router();
 
 router.put('/filter', filter)
 
+router.get("/:userid/:eventid",userRegistered )
 
 module.exports = router
