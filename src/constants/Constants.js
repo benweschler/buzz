4c6fd044709b6d36039b9579c11ac6b36ee0fecd
Changheng Zhao <arthur2000@g.ucla.edu>
2022-12-02 11:16:21 -0800

    Improve tag icons.

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,24 +1,24 @@
-import {BiPaint} from "react-icons/bi";
-import {IoMusicalNotesOutline} from "react-icons/io5";
+import {CgClipboard} from "react-icons/cg";
+import {GrWorkshop} from "react-icons/gr";
 import {MdSportsFootball} from "react-icons/md";
 import {FaUserFriends} from "react-icons/fa";
 import {ImBooks} from "react-icons/im";
 import {BsMusicNoteBeamed} from "react-icons/bs";
 import {GiMaterialsScience} from "react-icons/gi";
-import {BiDollar} from "react-icons/bi";
+import {FaUserTie} from "react-icons/fa";
 import {GiHighHeel} from "react-icons/gi";
 import {RiHandCoinLine} from "react-icons/ri";
 
 export default class Constants {
   static tags = {
-    "Workshop": <IoMusicalNotesOutline/>,
-    "Info-Sesh": <BiPaint/>,
+    "Workshop": <GrWorkshop/>,
+    "Info-Sesh": <CgClipboard/>,
     "Sports": <MdSportsFootball/>,
     "Social": <FaUserFriends/>,
     "Humanities": <ImBooks/>,
     "Music": <BsMusicNoteBeamed/>,
     "STEM": <GiMaterialsScience/>,
-    "Career": <BiDollar/>,
+    "Career": <FaUserTie/>,
     "Formal": <GiHighHeel/>,
     "Fundraiser": <RiHandCoinLine/>
   };
